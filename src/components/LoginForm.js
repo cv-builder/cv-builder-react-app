8ccb78dcaaa0e0f0c53759601ad1d0fc46246856
IgorSwatowski <igi66@wp.pl>
2020-06-17 12:34:13 +0200

    Start create LoginForm

---
+++ src/components/LoginForm.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class LoginForm extends Component {
+    render(){
+        return(
+            <p>Here will be a login form in React</p>
+        )
+    }
+}
