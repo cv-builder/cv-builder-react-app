598504de285e4c5b7169dacbbd04b7fb3781f9f1
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-06-21 17:07:45 +0200

    Create personal data class with constructor

---
+++ src/components/objects/PersonalData.js
@@ -0,0 +1,18 @@
+class PersonalData {
+
+    constructor() {
+        this.firstName = '';
+        this.secondName = '';
+        this.lastName = '';
+        this.email = '';
+        this.phoneNumber = '';
+        this.profession = '';
+        this.linkedIn = '';
+        this.github = '';
+        this.twitter = '';
+        this.website = '';
+        this.address = '';
+    }
+}
+
+export default PersonalData;
