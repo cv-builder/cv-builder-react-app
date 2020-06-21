9c02d1b387eae64370886c0891f33a8a9eb5ab17
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-06-21 11:09:02 +0200

    Create endpoints and routes files

---
+++ src/endpoints/index.js
@@ -0,0 +1,19 @@
+const host = 'http://localhost:8080/api/';
+
+export const endpoints ={
+
+    login: 'http://localhost:8080/login',
+    course: host+'cv/course/',
+    education: host+'cv/education/',
+    experience: host+'cv/experience/',
+    interest: host+'cv/interest/',
+    personalData: host+'cv/personal-data/',
+    project: host+'cv/project/',
+    skill: host+'cv/skill/',
+    cv: host+'cv/',
+    user: host+'user/',
+    userCv: host+'user/cv/',
+    newPassword: host+'new-password',
+    passwordReset: host+'password-reset/'
+   
+}
---
+++ src/routes/index.js
@@ -0,0 +1,32 @@
+
+export const routes = {
+
+    mainPage: '/',
+    formPage: '/create',
+
+    course: '/cv/course',
+    courseId: '/cv/course/:id',
+    education: '/cv/education',
+    educationId: '/cv/education/:id',
+    experience: '/cv/experience/',
+    experienceId: '/cv/experience/:id',
+    interest: '/cv/interest',
+    interestId: '/cv/interest/:id',
+    personalData: '/cv/personal-data',
+    personalDataId: '/cv/personal-data/:id',
+    project: '/cv/project',
+    projectId: '/cv/project/:id',
+    skill: '/cv/skill',
+    skillId: '/cv/skill/:id',
+    cv: '/cv',
+    cvId: '/cv/:id',
+    user: '/account',
+    userId: '/user/:id',
+    userCv: '/user/cv/:id',
+
+    newPassword: '/new-password',
+    passwordReset: '/password-reset/:id',
+
+    confirmToken: '/token',
+
+}
