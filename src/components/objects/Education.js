0695608e826689b0f5302ad929dc5e5bcf4b3c8f
dominikjaniga91 <dominikjaniga91@gmail.com>
2020-06-21 20:54:07 +0200

    Create education class with constructor

---
+++ src/components/objects/Education.js
@@ -0,0 +1,15 @@
+class Education {
+
+    constructor() {
+        this.id = 0;
+        this.school = '';
+        this.city = '';
+        this.startDate = '';
+        this.finishDate = '';
+        this.degree = '';
+        this.specialization = '';
+        this.description = '';
+    }
+}
+
+export default Education;
