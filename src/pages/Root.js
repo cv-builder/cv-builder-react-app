6e5bfe5b638c101a0ce1ee63eb1445835dccdba9
IgorSwatowski <igi66@wp.pl>
2020-06-24 14:52:50 +0200

    Rewrite class component to function component

---
+++ src/pages/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+import LoginForm from '../views/LoginForm.js';
+
+
+function Root() {
+  return (
+      <LoginForm/>
+  );
+}
+
+export default Root;
